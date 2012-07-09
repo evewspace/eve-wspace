df58900594deb65e541e917b5e041a490859d3f6
Andrew Austin <acaustin@uark.edu>
2012-07-09 05:37:29 +0000

    Add eveapi cache handler to API.utils
    Added a cache handler for eveapi API library to API.utils. Modified
    settings to use tz-aware datetimes. Added unix timestamp_to_datetime
    function in API.utils. Added new version of eveapi library as submodule.

---
+++ evewspace/API/utils.py
@@ -0,0 +1,35 @@
+from evewspace.API.models import APICachedDocument
+from datetime import datetime
+import time, pytz
+
+def store(host, path, params, doc, obj):
+	"""Store an API document in our cache."""
+	#First remove any outdated versions of the document.
+	APICachedDocument.objects.filter(host=host, params=params, path=path).delete()
+
+	#Create a new cache entry
+	newDoc = APICachedDocument(host=host, params=params, path=path, xml=doc, cacheduntil=timestamp_to_datetime(obj.cachedUntil))
+	newDoc.save()
+
+def retrieve(host, path, params):
+	"""Get an API document from our cache."""
+	#Look for a valid API document in the cache, make sure it isn't stale and return it.
+	try:
+		document = APICachedDocument.objects.get(host=host, path=path, params=params)
+		if document.cacheduntil > datetime.utcnow().replace(tzinfo=pytz.utc):
+			return document.xml
+		else:
+			return None
+	except APICachedDocument.DoesNotExist:
+		#No document found, return None and let eveapi fetch and store one.
+		return None
+	except APICachedDocument.MultipleObjectsReturned:
+		#It shouldn't be possible for this to happen, so let's just return None and let it get sorted in store()
+		return None
+
+def timestamp_to_datetime(timestamp):
+	"""Converts a UNIX Timestamp (in UTC) to a python DateTime"""
+	result = datetime.fromtimestamp(timestamp).replace(tzinfo=pytz.utc)
+	return result
+
+
