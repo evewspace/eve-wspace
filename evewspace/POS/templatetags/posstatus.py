7b6abcc072db2fc86785b5d6ede5b9d978f9648c
Andrew Austin <acaustin@uark.edu>
2012-07-10 06:26:14 +0000

    Add posstatus template tags, core template tags.
    Added template tags for generating POS status lists. Added core template
    tags library for tags that operate on the SDD. Adjusted home.html
    template to include the new POS status list tag.

---
+++ evewspace/POS/templatetags/posstatus.py
@@ -0,0 +1,29 @@
+from django import template
+from POS.models import CorpPOS
+from django.contrib.auth.models import User
+from POS.utils import add_status_info
+register=template.Library()
+
+@register.inclusion_tag('posstatus.html')
+def myposstatus(user):
+	#Get list of POSes where user is the manager
+	poses = CorpPOS.objects.filter(manager=user)
+	#Get status information and return
+	posstatus = add_status_info(poses)
+	return {'posstatus': posstatus}
+
+@register.inclusion_tag('posstatus.html')
+def corpposstatus(user):
+	#If we have the 'can_see_all_pos' permission, show all corp POSes.
+	#If we do not, then only show those with manager = None
+	if user.has_perm('POS.can_see_all_pos'):
+		poses = CorpPOS.objects.all()
+	else:
+		poses = CorpPOS.objects.filter(manager=None)
+	#Add status info and return
+	posstatus = add_status_info(poses)
+	return {'posstatus': posstatus}
+
+@register.inclusion_tag('posstatus_detail.html')
+def posstatusdetails(posstatus):
+	return {'pos': posstatus}
---
+++ evewspace/core/templatetags/coretags.py
@@ -0,0 +1,14 @@
+from django import template
+from core.models import *
+
+register = template.Library()
+
+@register.simple_tag()
+def typename(typeid):
+	try:
+		return Type.objects.get(id=typeid).name
+	except Type.DoesNotExist:
+		return ''
+	except Type.MultipleObjectsReturned:
+		return ''
+	
