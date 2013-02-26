7d6df1b11426949820d9598f8c1b3ef621e140a9
Andrew Austin <acaustin@uark.edu>
2013-02-26 04:05:45 +0000

    Change load DB scripts to use South migrations.

--- puppet/scripts/django_load_db.sh
+++ puppet/scripts/django_load_db.sh
@@ -5,6 +5,7 @@ bunzip2 /home/vagrant/mysql55-inferno12-extended.sql.bz2
 mysql -u root -D djangotest < /home/vagrant/mysql55-inferno12-extended.sql
 rm /home/vagrant/mysql55-inferno12-extended.sql
 /vagrant/evewspace/manage.py syncdb --noinput
+/vagrant/evewspace/manage.py migrate
 /vagrant/evewspace/manage.py buildsystemdata
 /vagrant/evewspace/manage.py loaddata /vagrant/evewspace/*/fixtures/*.json
 /vagrant/evewspace/manage.py resetadmin
--- puppet/ubuntu_sample/files/load_db.sh
+++ puppet/ubuntu_sample/files/load_db.sh
@@ -2,6 +2,7 @@
 mysql -u root -e "create database evewspace;"
 mysql -u root -D evewspace < /home/evewspace/staticdata.sql
 /home/evewspace/evewspace/evewspace/manage.py syncdb --noinput
+/home/evewspace/evewspace/evewspace/manage.py migrate
 /home/evewspace/evewspace/evewspace/manage.py buildsystemdata
 /home/evewspace/evewspace/evewspace/manage.py loaddata /home/evewspace/evewspace/evewspace/*/fixtures/*.json
 /home/evewspace/evewspace/evewspace/manage.py resetadmin
