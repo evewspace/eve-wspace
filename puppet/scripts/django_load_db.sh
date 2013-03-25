eba385b8fad8b4e58eaf859c8e40b31c59e9057c
Andrew Austin <acaustin@uark.edu>
2013-03-25 11:21:14 +0000

    Change load_db scripts to use new defaults system.

--- puppet/scripts/django_load_db.sh
+++ puppet/scripts/django_load_db.sh
@@ -8,5 +8,6 @@ rm /home/vagrant/mysql55-inferno12-extended.sql
 /vagrant/evewspace/manage.py migrate
 /vagrant/evewspace/manage.py buildsystemdata
 /vagrant/evewspace/manage.py loaddata /vagrant/evewspace/*/fixtures/*.json
+/vagrant/evewspace/manage.py defaultsettings
 /vagrant/evewspace/manage.py resetadmin
 /vagrant/evewspace/manage.py syncrss
--- puppet/ubuntu_sample/files/load_db.sh
+++ puppet/ubuntu_sample/files/load_db.sh
@@ -5,5 +5,6 @@ mysql -u root -D evewspace < /home/evewspace/staticdata.sql
 /home/evewspace/evewspace/evewspace/manage.py migrate
 /home/evewspace/evewspace/evewspace/manage.py buildsystemdata
 /home/evewspace/evewspace/evewspace/manage.py loaddata /home/evewspace/evewspace/evewspace/*/fixtures/*.json
+/home/evewspace/evewspace/evewspace/manage.py defaultsettings
 /home/evewspace/evewspace/evewspace/manage.py resetadmin
 /home/evewspace/evewspace/evewspace/manage.py syncrss
