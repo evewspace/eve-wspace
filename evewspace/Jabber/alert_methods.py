5df4e8a7fc744a9e3686723231bfe16223713de3
acdervis <acdervis3@gmail.com>
2014-08-29 16:43:06 +0300

    Added Slack alert integration. Migrations need to be updated.

--- evewspace/Jabber/alert_methods.py
+++ evewspace/Jabber/alert_methods.py
@@ -1,3 +1,5 @@
 from Alerts.method_registry import registry
 from jabber_method import JabberAlertMethod
+from slack_method import SlackAlertMethod
 registry.register("Jabber", JabberAlertMethod)
+registry.register("Slack", SlackAlertMethod)
