8e7b93d37789bafc885c76673b65e49439b23b06
acdervis <acdervis3@gmail.com>
2014-12-18 17:03:45 +0200

    Changed eve-scout conditional to use id instead of class for future-proofing.

--- evewspace/Map/models.py
+++ evewspace/Map/models.py
@@ -132,10 +132,6 @@ class System(SystemData):
         if self.sysclass == 13:
             return True
 
-    def is_thera(self):
-        if self.sysclass == 12:
-            return True
-
     def get_spec(self):
         if self.sysclass in range(7,12):
             return self.ksystem
