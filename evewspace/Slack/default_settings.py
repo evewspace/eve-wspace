2a0042054726d2f1b6e904e3b29c373b18c261d0
Jaakko Kantojärvi <jaakko@n-1.fi>
2016-02-16 19:56:18 +0200

    Change defaultsettings to keep existing settings

--- evewspace/Slack/default_settings.py
+++ evewspace/Slack/default_settings.py
@@ -1,14 +1,23 @@
-from core.models import ConfigEntry
-import random
-import string
-#defaults = [("TEST_SETTING", "BOB")]
+#   Eve W-Space
+#   Copyright 2014 Andrew Austin and contributors
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+from core.utils import load_defaults
+from functools import partial
+
 defaults = [
         ("SLACK_ENABLED", False),
         ("SLACK_SUBDOMAIN", "SLACK"),
         ]
 
-def load_defaults():
-    for setting in defaults:
-        config = ConfigEntry.objects.get_or_create(name=setting[0], user=None)[0]
-        config.value = setting[1]
-        config.save()
+load_defaults = partial(load_defaults, defaults)
