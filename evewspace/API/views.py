c55c97825020776e09b1c1e26a9c42084236012b
Maarten28 <maarten28@gmail.com>
2016-12-19 21:38:50 +0100

    Adding CREST related views

--- evewspace/API/views.py
+++ evewspace/API/views.py
@@ -13,16 +13,22 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+from datetime import datetime
 import pytz
+import urllib2
+import json
+import base64
 
 from django.core.exceptions import PermissionDenied
-from django.http import Http404, HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.template.response import TemplateResponse
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
-from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import permission_required, login_required
+from django.conf import settings
 
-from API.models import CorpAPIKey, MemberAPIKey, APIKey, APIShipLog, APICharacter
+from API.models import CorpAPIKey, MemberAPIKey, APIKey, APIShipLog, APICharacter, CRESTRefreshToken
+from API.utils import crest_refresh_access_token, crest_access_data, crest_verify
 import API.cache_handler as handler
 
 User = get_user_model()
@@ -124,3 +130,67 @@ def delete_corp_key(request, key_id):
     api_key = get_object_or_404(CorpAPIKey, keyid=key_id)    
     api_key.delete()
     return HttpResponse()
+
+@login_required()
+def crest_login(request):
+    if not settings.CREST_ENABLED:
+        raise PermissionDenied
+    if request.GET.get('code'): 
+    
+        #use code to get access & refresh token
+        authorization = base64.urlsafe_b64encode(settings.CREST_CLIENT_ID + ':' + settings.CREST_SECRET_KEY)
+        data = 'grant_type=authorization_code&code=' + request.GET.get('code')
+        url = 'https://'+settings.CREST_LOGIN_SERVER+'/oauth/token'
+        headers = {'Content-Type': 'application/x-www-form-urlencoded',
+            'Host': settings.CREST_LOGIN_SERVER,
+            'Authorization': 'Basic '+ authorization,}
+        opener = urllib2.build_opener(urllib2.HTTPHandler)
+        requested = urllib2.Request(url, data, headers)
+        json_response = opener.open(requested).read()
+        access_response = json.loads(json_response)
+        
+        #verify validity by requesting char info
+        char_authorization = access_response['access_token']
+        char_url = 'https://'+settings.CREST_LOGIN_SERVER+'/oauth/verify'
+        char_headers = {'User-Agent': settings.CREST_USER_AGENT,
+            'Host': settings.CREST_LOGIN_SERVER,
+            'Authorization': 'Bearer '+ char_authorization,}
+        char_requested = urllib2.Request(char_url, None, char_headers)
+        char_json_response = opener.open(char_requested).read()
+        
+        char_response = json.loads(char_json_response)
+        
+        updated_values = {
+            'user_id': request.user.pk,
+            'refresh_token': access_response['refresh_token'],
+            'access_token': access_response['access_token'],
+            'valid_until': char_response["ExpiresOn"],
+            'char_name': char_response["CharacterName"],
+        }
+        token_info = CRESTRefreshToken.objects.update_or_create(
+            char_id=char_response['CharacterID'], defaults=updated_values)
+        
+        
+        return HttpResponseRedirect('/api/crest/overview/')
+    return HttpResponseRedirect('https://'+settings.CREST_LOGIN_SERVER+'/oauth/authorize/?response_type=code&redirect_uri='+settings.CREST_BASE_URL+'api/crest/login/&client_id='+ settings.CREST_CLIENT_ID +'&scope='+settings.CREST_SCOPE+'&state=') # 302
+
+@login_required()
+def crest_overview(request):
+    if not settings.CREST_ENABLED:
+        return TemplateResponse(request, 'crest_not_enabled.html')
+    
+    tokens = CRESTRefreshToken.objects.filter(user=request.user).all()
+    data = []
+    
+    for token in tokens:
+        #request scope and verify validity by requesting char info
+        response = crest_verify(token)
+        data.append(response)
+
+    return TemplateResponse(request, 'crest_overview.html', {'tokens': data})
+    
+@login_required()
+def crest_delete(request, char_id):
+    CRESTRefreshToken.objects.filter(user=request.user, char_id=char_id).delete()
+    
+    return HttpResponseRedirect('/api/crest/overview/')
