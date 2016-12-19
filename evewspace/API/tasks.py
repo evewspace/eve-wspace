bc8978e028d8a93056a6a1bece723963dd63db57
Maarten28 <maarten28@gmail.com>
2016-12-19 21:36:54 +0100

    Adding update char location CREST task

--- evewspace/API/tasks.py
+++ evewspace/API/tasks.py
@@ -14,10 +14,20 @@
 #   limitations under the License.
 
 from celery import task
-from API.models import APIKey, MemberAPIKey
+from datetime import datetime
+from API.models import APIKey, MemberAPIKey, CRESTRefreshToken
+from Map.models import System
+from API.utils import crest_refresh_access_token, crest_access_data
 from django.core.cache import cache
 from django.contrib.auth import get_user_model
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+
+import urllib2
+import json
+import base64
 import sys
+import pytz
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
@@ -31,3 +41,33 @@ def update_char_data():
         #Get all API keys of a user and validate them
         for key in user.api_keys.all():
             key.validate()
+
+@task()
+def update_char_location():
+    for token in CRESTRefreshToken.objects.all():
+        response = crest_access_data(token,'/characters/'+token.char_id+'/location/')
+        
+        if response: 
+	        system_pk = response["solarSystem"]["id"]
+	        char_cache_key = 'char_%s_location' % token.char_id
+	        old_location = cache.get(char_cache_key)
+	        print old_location
+	        current_system = get_object_or_404(System, pk=system_pk)
+	        print current_system
+	        if old_location != current_system:
+	            if old_location:
+	                old_system = get_object_or_404(System, name=old_location)
+	                old_system.remove_active_pilot(token.char_id)
+	            token.user.update_location(
+	                current_system.pk,
+	                token.char_id, token.char_name,
+	                "CREST", "CREST")
+	        
+	        cache.set(char_cache_key, current_system, 60 * 5)
+	        # Use add_active_pilot to refresh the user's record in the global
+	        # location cache
+	        current_system.add_active_pilot(
+	            token.user, token.char_id, token.char_name,
+	            "CREST", "CREST"
+	        )
+        
