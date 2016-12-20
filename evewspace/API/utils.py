3fe55a5587e1242336898d069110e0b16da2b2f7
Maarten28 <maarten28@gmail.com>
2016-12-20 03:31:39 +0100

    Renaming to SSO and including basic ESI support

--- evewspace/API/utils.py
+++ evewspace/API/utils.py
@@ -13,30 +13,35 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 from datetime import datetime
-from API.models import CRESTRefreshToken
+from API.models import SSORefreshToken
 from django.conf import settings
 
 import pytz
 import urllib2
 import json
 import base64
+import pycurl
+from io import BytesIO
+data = BytesIO()
+
+
 
 def timestamp_to_datetime(timestamp):
     """Converts a UNIX Timestamp (in UTC) to a python DateTime"""
     result = datetime.fromtimestamp(timestamp).replace(tzinfo=pytz.utc)
     return result
 
-def crest_refresh_access_token(char_id):
-    refresh_token = CRESTRefreshToken.objects.get(
+def sso_refresh_access_token(char_id):
+    refresh_token = SSORefreshToken.objects.get(
     		char_id=char_id)
 
     if refresh_token.valid_until < datetime.now(pytz.utc):
 	    #use code to get access & refresh token
-	    authorization = base64.urlsafe_b64encode(settings.CREST_CLIENT_ID + ':' + settings.CREST_SECRET_KEY)
+	    authorization = base64.urlsafe_b64encode(settings.SSO_CLIENT_ID + ':' + settings.SSO_SECRET_KEY)
 	    data = 'grant_type=refresh_token&refresh_token=' + refresh_token.refresh_token
-	    url = 'https://'+settings.CREST_LOGIN_SERVER+'/oauth/token'
+	    url = 'https://'+settings.SSO_LOGIN_SERVER+'/oauth/token'
 	    headers = {'Content-Type': 'application/x-www-form-urlencoded',
-	        'Host': settings.CREST_LOGIN_SERVER,
+	        'Host': settings.SSO_LOGIN_SERVER,
 	        'Authorization': 'Basic '+ authorization,}
 	    opener = urllib2.build_opener(urllib2.HTTPHandler)
 	    requested = urllib2.Request(url, data, headers)
@@ -45,9 +50,9 @@ def crest_refresh_access_token(char_id):
 	    
 	    #verify validity by requesting char info
 	    char_authorization = access_response['access_token']
-	    char_url = 'https://'+settings.CREST_LOGIN_SERVER+'/oauth/verify'
-	    char_headers = {'User-Agent': settings.CREST_USER_AGENT,
-	        'Host': settings.CREST_LOGIN_SERVER,
+	    char_url = 'https://'+settings.SSO_LOGIN_SERVER+'/oauth/verify'
+	    char_headers = {'User-Agent': settings.SSO_USER_AGENT,
+	        'Host': settings.SSO_LOGIN_SERVER,
 	        'Authorization': 'Bearer '+ char_authorization,}
 	    char_requested = urllib2.Request(char_url, None, char_headers)
 	    char_json_response = opener.open(char_requested).read()
@@ -58,21 +63,22 @@ def crest_refresh_access_token(char_id):
 	    refresh_token.valid_until = char_response["ExpiresOn"] 
 	    refresh_token.save()
 
-	    refresh_token = CRESTRefreshToken.objects.get(
+	    refresh_token = SSORefreshToken.objects.get(
     		char_id=char_response['CharacterID'])
 
     return refresh_token
     
 def crest_access_data(token, requested_url, post_data = None):
     if token.valid_until < datetime.now(pytz.utc):
-        token = crest_refresh_access_token(token.char_id)
-    if settings.CREST_ENABLED == False:
+        token = sso_refresh_access_token(token.char_id)
+    if settings.SSO_ENABLED == False:
 	    return None
     authorization = token.access_token
-    url = 'https://'+settings.CREST_AUTH_SERVER+requested_url
-    headers = {'User-Agent': settings.CREST_USER_AGENT,
-        'Host': settings.CREST_AUTH_SERVER,
-        'Authorization': 'Bearer '+ authorization,}
+    url = 'https://'+settings.CREST_SERVER+requested_url
+    headers = {'User-Agent': settings.SSO_USER_AGENT,
+        'Host': settings.CREST_SERVER,
+        'Authorization': 'Bearer '+ authorization,
+        'Accept': 'application/json',}
     opener = urllib2.build_opener(urllib2.HTTPHandler)
     requested = urllib2.Request(url, post_data, headers)
     open_page = opener.open(requested)
@@ -85,15 +91,15 @@ def crest_access_data(token, requested_url, post_data = None):
     return None
     
     
-def crest_verify(token):
+def sso_verify(token):
     if token.valid_until < datetime.now(pytz.utc):
-        token = crest_refresh_access_token(token.char_id)
+        token = sso_refresh_access_token(token.char_id)
     
     opener = urllib2.build_opener(urllib2.HTTPHandler)
     authorization = token.access_token
-    url = 'https://'+settings.CREST_LOGIN_SERVER+'/oauth/verify'
-    headers = {'User-Agent': settings.CREST_USER_AGENT,
-        'Host': settings.CREST_LOGIN_SERVER,
+    url = 'https://'+settings.SSO_LOGIN_SERVER+'/oauth/verify'
+    headers = {'User-Agent': settings.SSO_USER_AGENT,
+        'Host': settings.SSO_LOGIN_SERVER,
         'Authorization': 'Bearer '+ authorization,}
     requested = urllib2.Request(url, None, headers)
     json_response = opener.open(requested).read()
@@ -103,3 +109,17 @@ def crest_verify(token):
     if response:
        return response
     return None
+    
+def esi_access_data(token, requested_url, call_type = None, post_data = None):
+    authorization = token.access_token
+    url = 'https://'+ settings.ESI_SERVER + '/' + requested_url + '?datasource=' + settings.ESI_SOURCE
+    curl = pycurl.Curl()
+    curl.setopt(curl.URL, url)
+    curl.setopt(curl.HTTPHEADER,['Content-Type: application/json','Authorization:' 'Bearer '+ authorization])
+    curl.setopt(curl.READFUNCTION, data.write)
+    curl.setopt(curl.TIMEOUT, 5)
+    curl.setopt(curl.VERBOSE, True)
+    curl.perform()
+    
+    return data
+
