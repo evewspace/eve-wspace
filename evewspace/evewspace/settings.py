d15e0a435dac612aee31e414af0fdf70d0ce31d1
Maarten28 <maarten28@gmail.com>
2016-12-20 21:52:42 +0100

    Add CREST characterLocationRead scope

--- evewspace/evewspace/settings.py
+++ evewspace/evewspace/settings.py
@@ -249,7 +249,7 @@ SSO_CLIENT_ID = 'xxxxxxx' #to be created at https://developers.eveonline.com/ fo
 SSO_BASE_URL = 'xxxxxx' #do not forget the / at the end and https if the connection is secure
 SSO_LOGIN_SERVER = 'login.eveonline.com' #check http://eveonline-third-party-documentation.readthedocs.io/en/latest/reference/reference.html for sisi
 #'esi-location.read_location.v1 esi-location.read_ship_type.v1' are needed for location and ship type information
-SSO_SCOPE = 'esi-location.read_location.v1 esi-location.read_ship_type.v1' #scope of authorizations requested
+SSO_SCOPE = 'esi-location.read_location.v1 esi-location.read_ship_type.v1 characterLocationRead' #scope of authorizations requested
 SSO_USER_AGENT = 'EVE W-space Instance' #Header
     
 #CREST
